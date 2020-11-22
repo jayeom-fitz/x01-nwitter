6e500accf1c51c13a8a1cfc989f799f032c45f52
JayEom <djawjdgus93@naver.com>
2020-11-22 21:38:08 +0900

    #2-5 LogOut

--- src/routes/Profile.js
+++ src/routes/Profile.js
@@ -1,4 +1,20 @@
 import React from "react";
+import { useHistory } from "react-router-dom";
+import { authService } from "../fbase";
 
-const Profile = () => <span>Profile</span>
+const Profile = () => {
+  const history = useHistory();
+
+  const onLogOutClick = () => {
+    authService.signOut();
+    history.push("/");
+  };
+
+  return (
+    <>
+      <button onClick={onLogOutClick}>LogOut</button>
+    </>
+  );
+};
+
 export default Profile;
